dd948fa35c98601039d975c89b5c293e0c7dd189
manuiitm <manuk8336@gmail.com>
2025-02-14 12:03:54 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
-int main() {
-    int a,b;
-     scanf("%d",&a);
-    scanf("%d",&b);
-    printf("a = %d\n", b);
-    printf("b = %d\n", a);
+#include <stdio.h>  
+
+void swap(int *a, int *b);      
+
+int main() {                   
+    int a ;
+    int b ;
+    scanf("%d %d",&a,&b);
+    swap(&a, &b);          
+    printf("a = %d\nb = %d", a, b);  
     return 0;
+} 
+
+void swap(int *a, int *b) {
+    int temp = *a;  // Store value of a
+    *a = *b;        // Assign value of b to a
+    *b = temp;      // Assign stored value of a to b
 }
